f1ae6fcb8fc88f3aca5a08ba7d70f0911bd054c2
Abhijeet Verma <Abhijeet Verma>
2021-09-17 13:07:28 +0530

    End of Section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]   //---->route to the api
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
