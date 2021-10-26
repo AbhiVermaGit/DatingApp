89491238a7a29fe0017d39cbca6e046f8cf7fec5
Abhijeet Verma <Abhijeet Verma>
2021-10-26 13:00:19 +0530

    End of section 10

---
+++ client/src/app/_services/busy.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  busyRequestCount = 0;
+
+  constructor(private spinnerService: NgxSpinnerService) { }
+
+
+
+  busy(){
+    this.busyRequestCount++;
+    this.spinnerService.show(undefined,{
+      type: 'ball-fussion',
+      bdColor: 'rgba(255,255,255,0)',
+      color: '#e87e0c'
+    });
+  }
+
+  idle(){
+    this.busyRequestCount--;
+    if(this.busyRequestCount <=0){
+      this.busyRequestCount = 0;
+      this.spinnerService.hide();
+    }
+  }
+
+}
